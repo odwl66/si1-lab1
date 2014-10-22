a6de7846ddeb4e554142bcaed96611461baf24c5
odwl66 <orion.lima@ccc.ufcg.edu.br>
2014-10-22 09:40:52 -0300

    Quase pronto, mas ainda bugado

---
+++ dinamica/js/script_da_terceira_pagina.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Órion on 21/10/2014.
+ */
+
+
+function adicionarAListaParaAprender(){
+    item = $("#input_assunto").val();
+    mover = "<button onclick='moverParaAprendidos()' class='btn btn-warning'> Mover para aprendidos</button>";
+    remover = "<button onclick='removerDaPagina()' class='btn btn-danger'> Remover</button>";
+    $("#listaDeTemasAAprender").append("<li class='list-group-item'>"+item+mover+remover+"</li>");
+    return false;
+}
+
+function moverParaAprendidos(){
+    $( document ).on( "click", function( event ) {
+        tema = $( event.target ).closest( "li")[0].childNodes[0].data;
+        removerDaPagina();
+        $("#listaDeTemasJaAprendido").append("<li class='list-group-item'>" +tema+ "</li>");
+    });
+}
+
+function removerDaPagina(){
+    $( document ).on( "click", function( event ) {
+        $( event.target ).closest( "li" ).remove();
+    });
+}
